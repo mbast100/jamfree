be0431c5af4a14d9c1f7a6642bd75b644393fd4b
Amir Fanous <afano056@uottawa.ca>
2021-02-07 12:47:06 -0500

    QR page update

--- front-end/src/components/Home/components/Navigation/index.js
+++ front-end/src/components/Home/components/Navigation/index.js
@@ -84,10 +84,8 @@ export default function Navigation(props) {
         <Box my={2}>
           {[...new Array(10)]
             .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              () => `Welcome to the JamFam! Thank you for helping save lives!
+              Please scan this QR code upon arrival to your favourtie restaurant! Enjoy your visit and stay safe :)`
             )
             .join("\n")}
         </Box>
